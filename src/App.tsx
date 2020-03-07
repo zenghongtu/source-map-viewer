cd6a6bf41b2485e29e312d0484a4a3367b6de771
zenghongtu <zenghongtu@gmail.com>
2020-03-07 14:53:15 +0800

    init project

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+
+class App extends React.Component {
+  render() {
+    return <div>app</div>;
+  }
+}
+
+ReactDOM.render(<App></App>, document.getElementById('app'));
